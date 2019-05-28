03190535c0ea21dd46ff7809c0b998e5897fd6bc
Erick Olibo <erick@defkut.com>
2019-05-28 00:03:16 -0400

    Initial Commit

---
+++ FawGen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FawGen
+//
+//  Created by Erick Olibo on 28/05/2019.
+//  Copyright © 2019 DEFKUT Creations OU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
