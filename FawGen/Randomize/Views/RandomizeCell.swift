531f443f69bf05f3e2c137048cba66f7346a3783
Erick Olibo <erick@defkut.com>
2019-06-03 22:56:01 -0400

    imported icons for the navigation bar and cells

---
+++ FawGen/Randomize/Views/RandomizeCell.swift
@@ -0,0 +1,29 @@
+//
+//  RandomizeCell.swift
+//  FawGen
+//
+//  Created by Erick Olibo on 03/06/2019.
+//  Copyright © 2019 DEFKUT Creations OU. All rights reserved.
+//
+
+import UIKit
+
+class RandomizeCell: UITableViewCell {
+    
+    // MARK: - Properties
+    static var identifier: String { return String(describing: self) }
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
