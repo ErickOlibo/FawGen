8d091ff752b43cc4e086257d6c8ecb0d22839d02
Erick Olibo <erick@defkut.com>
2019-07-30 20:07:13 +0300

    simple refactoring

--- FawGen/Model View Controllers/Randomize/Controller/RandomizeVCExtension.swift
+++ FawGen/Model View Controllers/Randomize/Controller/RandomizeVCExtension.swift
@@ -8,6 +8,234 @@
 
 import UIKit
 
+
+extension RandomizeViewController {
+    
+    /// Place to get notification for UIApplication Active state
+    public func applicationNotificationHandler(_ state: ObserverState) {
+        
+        switch state {
+        case .add:
+            NotificationCenter.default.addObserver(self, selector: #selector(applicationWillResignActive), name: UIApplication.willResignActiveNotification, object: nil)
+            NotificationCenter.default.addObserver(self, selector: #selector(applicationDidBecomeActive), name: UIApplication.didBecomeActiveNotification, object: nil)
+        //print("add")
+        case .remove:
+            NotificationCenter.default.removeObserver(self, name: UIApplication.willResignActiveNotification, object: nil)
+            NotificationCenter.default.removeObserver(self, name: UIApplication.didBecomeActiveNotification, object: nil)
+            //print("remove")
+        }
+        
+    }
+    
+    /// Place to remove or add keyboard notification handler
+    public func keyboardNotificationHandler(_ state: ObserverState) {
+        
+        switch state {
+        case .add:
+            // Add observers
+            NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+            NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+            NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidHide), name: UIResponder.keyboardDidHideNotification, object: nil)
+        case .remove:
+            // Remove Observers
+            NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+            NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+            NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardDidHideNotification, object: nil)
+            
+        }
+    }
+    
+    /// Selects the right TableFooterView to present. Depending on the
+    /// size of the DataSource Items count. If 0 then AssistHome, else
+    /// NewSetHome
+    public func simpleAssistOrNewSetHomeUI() {
+        if dataSource.items.count > 0 {
+            tableView.isScrollEnabled = true
+            let viewBounds = CGRect(x: 0, y: 0, width: tableView.bounds.width, height: 60)
+            let newSetHomeView = NewSetHomeView(frame: viewBounds)
+            newSetHomeView.newSetHomeDelegate = self
+            tableView.tableFooterView = newSetHomeView
+            
+        } else {
+            let simpleAssistView = SimpleAssistView(frame: tableView.bounds)
+            simpleAssistView.simpleAssistDelegate = self
+            tableView.tableFooterView = simpleAssistView
+            tableView.isScrollEnabled = false
+            printConsole("[FooterView Bounds] - \(tableView.tableFooterView!.bounds)")
+        }
+    }
+}
+
+// MARK: - Application Active Notifications
+extension RandomizeViewController {
+    @objc func applicationWillResignActive(_ notification : NSNotification) {
+        if let simpleAssistView = tableView.tableFooterView as? SimpleAssistView {
+            printConsole("RESIGN SimpleAssistView Keyboard from RandomView")
+            simpleAssistView.keywordsGrowningTextView.resignFirstResponder()
+        }
+        keyboardNotificationHandler(.remove)
+        printConsole("[RandomizeVCExtension] ==> applicationWillResignActive")
+    }
+    
+    @objc func applicationDidBecomeActive(_ notification : NSNotification) {
+        keyboardNotificationHandler(.add)
+        printConsole("[RandomizeVCExtension] ==> applicationDidBecomeActive")
+    }
+    
+    
+}
+
+
+
+
+extension RandomizeViewController {
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath) as! FakeWordCell
+        cell.setDomainSocialViewsToNormal()
+        cell.state = .opened
+        dataSource.addOpenedIndexPath(indexPath)
+        
+        tableView.beginUpdates()
+        cell.bottomView.alpha = 0
+        UIView.animate(withDuration: 0.7) {
+            cell.bottomView.alpha = 1
+        }
+        tableView.endUpdates()
+        cell.queryDomainSocialChecker()
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath) as! FakeWordCell
+        cell.state = .closed
+        dataSource.removeOpenedIndexPath(indexPath)
+        
+        tableView.beginUpdates()
+        cell.bottomView.alpha = 1
+        UIView.animate(withDuration: 0.7) {
+            cell.bottomView.alpha = 0
+        }
+        tableView.endUpdates()
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        printConsole("WILL SELECT at row: \(indexPath.row)")
+        if reachability.networkStatus() == .unavailable {
+            if let controller = reachability.internetConnectionAlertController() {
+                present(controller, animated: true, completion: nil)
+            }
+            return nil
+        } else {
+            return indexPath
+        }
+    }
+    
+    
+}
+
+
+// Comforming to NewSetHomeDelegate
+extension RandomizeViewController: NewSetHomeDelegate {
+    func showSimpleAssist() {
+        prepareForShowingSimpleAssist()
+    }
+    
+    func queryNewSetFromSimpleModel() {
+        letsGoQuery(.simple)
+    }
+    
+    
+    private func prepareForShowingSimpleAssist() {
+        let newItems = dataSource.getEmptyItems()
+        tableView.beginUpdates()
+        let rowsCount = tableView.numberOfRows(inSection: 0)
+        let indexPaths = (0..<rowsCount).map { IndexPath(row: $0, section: 0)}
+        tableView.deleteRows(at: indexPaths, with: .automatic)
+        dataSource.items = newItems
+        tableView.endUpdates()
+        simpleAssistOrNewSetHomeUI()
+    }
+    
+    
+}
+
+
+// Conforming to SimpleAssistDelegate
+extension RandomizeViewController: SimpleAssistDelegate {
+    
+    func querySimpleModel() {
+        letsGoQuery(.simple)
+    }
+    
+    func queryAssistedModel(by keywords: String) {
+        letsGoQuery(.assist, with: keywords)
+    }
+    
+    
+    /// Queries the model after the user has pressed Let's Go button. Depending
+    /// on the type (simple or assit) a different Model Engine will be queried
+    /// - Parameters:
+    ///     - type: between simple and assist or without keywords or with
+    ///     - keywords: string entered by the user
+    private func letsGoQuery(_ type: LetsGoType, with keywords: String = String()){
+        tableView.tableFooterView = UIView(frame: CGRect.zero)
+        
+        // Get the fakeWords from the Model
+        switch type {
+        case .simple:
+            // Get the New Items from the DataShource to Display
+            // This is the random one
+            printConsole("Get X random words from model")
+        case .assist:
+            // Get the new Items from the Keyboards and vector space from
+            // Model
+            printConsole("With KEYWORDS, get X random words from model")
+        }
+        
+        // Variable to be replaces by words from model
+        // Used to be 20
+        let size = fontNamesList.count
+        let newItems = getNewRandomItems(count: size)
+        
+        tableView.beginUpdates()
+        let rowsCount = tableView.numberOfRows(inSection: 0)
+        let indexPaths = (0..<rowsCount).map { IndexPath(row: $0, section: 0)}
+        
+        tableView.deleteRows(at: indexPaths, with: .automatic)
+        dataSource.items = newItems
+        dataSource.indexPaths = Set<IndexPath>()
+        var indexPathsNew = [IndexPath]()
+        for idx in 0..<dataSource.items.count {
+            let path = IndexPath(row: idx, section: 0)
+            indexPathsNew.append(path)
+        }
+        tableView.insertRows(at: indexPathsNew, with: .automatic)
+        tableView.endUpdates()
+        
+        if let firstIndex = indexPathsNew.first {
+            tableView.scrollToRow(at: firstIndex, at: .top, animated: true)
+        }
+        simpleAssistOrNewSetHomeUI()
+    }
+    
+    /// Makes sure the words were not used before
+    private func getNewRandomItems(count: Int) -> [FakeWord] {
+        
+        return dataSource.getFontsToFakeword()
+        
+    }
+    
+}
+
+
+
+
+
+
+
+
 // MARK: - Navigation Bar
 extension RandomizeViewController {
     
